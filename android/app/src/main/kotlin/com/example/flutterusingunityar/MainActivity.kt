2543d275a8f495b439d1cf9c1f12c8468d963a1f
bassantAyman323 <74427049+bassantAyman323@users.noreply.github.com>
2022-02-15 00:02:34 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/example/flutterusingunityar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterusingunityar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'secondPage.dart';
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primarySwatch: Colors.blue,
+      ),
+      home:  MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            MaterialButton(child: Text("Go to unity test"),minWidth:double.infinity,
+              onPressed: () async {
+                Navigator.of(context).push((MaterialPageRoute(builder: (BuildContext context) => UnityTestingWrapper() )));
+              },),
+          ],
+        ),
+      ),
+    // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
